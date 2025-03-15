ca2fc2cdfca853a3d074f4d9733a66ba831f7952
GMKR <2089640+GMKR@users.noreply.github.com>
2025-03-15 20:38:23 +0530

    mark optional inputs

--- src/utils.ts
+++ src/utils.ts
@@ -3,9 +3,9 @@ import { useTogether } from './providers/together'
 
 export const InputSchema = {
   prompt: z.string().describe("The prompt to generate an image for"),
-  width: z.coerce.number().describe("The width of the image to generate"),
-  height: z.coerce.number().describe("The height of the image to generate"),
-  numberOfImages: z.coerce.number().describe("The number of images to generate").default(1),
+  width: z.coerce.number().describe("The width of the image to generate").optional(),
+  height: z.coerce.number().describe("The height of the image to generate").optional(),
+  numberOfImages: z.coerce.number().describe("The number of images to generate").optional(),
 }
 
 export const handler = async (prompt: string, params: Record<string, any>) => {
